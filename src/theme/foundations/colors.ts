3907a6b1556107650125bfdd9f669c423ef93802
ganm0r <gndhrvmv@gmail.com>
2022-03-13 00:38:38 +0530

    :lipstick: Add theme for components

---
+++ src/theme/foundations/colors.ts
@@ -0,0 +1,15 @@
+const colors = {
+  /** #2B2B2B */
+  "grey-1": "#2B2B2B",
+
+  /** #E5E5E5 */
+  "grey-0": "#E5E5E5",
+
+  /** #FFCDD2 */
+  "purple-0": "#FFCDD2",
+
+  /** #EF9A9A */
+  "purple-1": "#EF9A9A",
+};
+
+export default colors;
