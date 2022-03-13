7110c536abf798e0c52222c3362e45d046febeaa
ganm0r <gndhrvmv@gmail.com>
2022-03-13 12:55:01 +0530

    :fire: Remove Info from Input

--- src/components/Input/Input.tsx
+++ src/components/Input/Input.tsx
@@ -1,47 +1,38 @@
 import React from "react";
 import styled, { css } from "styled-components";
 import theme from "../../theme";
-import Icon from "../Icon/Icon";
 import { Stack } from "../Stack/Stack";
 
 const StyledInputTitle = styled.p(
   () => css`
     font-family: ${theme.fonts.primary};
     font-weight: ${theme.fontWeights.extrabold};
-    font-size: 16px;
+    font-size: 20px;
     letter-spacing: 0.15em;
     text-transform: uppercase;
-    color: ${theme.colors["purple-1"]};
+    color: ${theme.colors["purple-2"]};
     margin-top: 5%;
   `
 );
-
-const StyledInputInfo = styled.p`
-  font-family: ${theme.fonts.primary};
-  font-weight: ${theme.fontWeights.bold};
-  color: ${theme.colors["purple-1"]};
-  font-size: 12px;
-  margin-top: 0;
-`;
 
 const StyledInputField = styled.input(
   () => css`
     border-style: solid;
     border-radius: 8px;
     border-width: 3px;
-    min-width: 400px;
+    min-width: 500px;
     padding: 12px;
     box-sizing: border-box;
     background-color: ${theme.colors["purple-0"]};
-    border-color: ${theme.colors["purple-1"]};
+    border-color: ${theme.colors["purple-2"]};
     font-family: ${theme.fonts.primary};
-    font-weight: ${theme.fontWeights.semibold};
+    font-weight: ${theme.fontWeights.bold};
     font-size: 18px;
-    color: ${theme.colors["purple-1"]};
+    color: ${theme.colors["grey-1"]};
 
     &:focus {
       outline: none;
-      border-color: ${theme.colors["purple-1"]};
+      border-color: ${theme.colors["purple-2"]};
     }
   `
 );
@@ -59,19 +50,12 @@ const Input = ({
   title,
   type,
   placeholder,
-  info,
   required,
   onChange,
 }: InputProps): JSX.Element => {
   return (
     <Stack direction="column" spacing={-2}>
       <StyledInputTitle>{title}</StyledInputTitle>
-      {info && (
-        <Stack direction="row" spacing={8}>
-          <Icon name="info" color="grey-0" size="extra-small" />
-          <StyledInputInfo>{info}</StyledInputInfo>
-        </Stack>
-      )}
       <StyledInputField
         type={type}
         placeholder={placeholder}
