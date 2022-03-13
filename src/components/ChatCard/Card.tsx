bdb8ba3b2d03e2e3c1c25649b01eba445bfc6ef3
ganm0r <gndhrvmv@gmail.com>
2022-03-13 21:45:59 +0530

    :sparkles: Add ChatCard component for messages

---
+++ src/components/ChatCard/Card.tsx
@@ -0,0 +1,60 @@
+import React from "react";
+import styled, { css } from "styled-components";
+import theme from "../../theme";
+
+const { colors } = theme;
+declare type Color = keyof typeof colors;
+
+interface StyledCardProps {
+  backgroundColor?: Color;
+  spacing?: number;
+}
+
+const StyledCard = styled.div<StyledCardProps>(
+  ({ backgroundColor, spacing }) => css`
+    background-color: ${backgroundColor ? colors[backgroundColor] : "white"};
+    border-radius: 18px;
+    box-sizing: border-box;
+    padding: 6px;
+    width: max-content;
+    margin: 2px;
+
+    & > *:not(:last-child) {
+      ${spacing
+        ? css`
+            margin-bottom: ${spacing}px;
+          `
+        : css`
+            margin-bottom: 22px;
+          `}
+    }
+  `
+);
+
+interface CardProps {
+  /** The background color of the Card */
+  backgroundColor?: Color;
+
+  /** The spacing between children (in px) */
+  spacing?: number;
+
+  /** The children inside the Card */
+  children?: React.ReactNode | React.ReactNode[];
+}
+
+const Card = ({
+  backgroundColor,
+  spacing,
+  children,
+}: CardProps): JSX.Element => {
+  return (
+    <React.Fragment>
+      <StyledCard backgroundColor={backgroundColor} spacing={spacing}>
+        {children}
+      </StyledCard>
+    </React.Fragment>
+  );
+};
+
+export { Card };
+export type { CardProps };
