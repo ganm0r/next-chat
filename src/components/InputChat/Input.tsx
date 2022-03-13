288891cb517dd274b5975f3cf2a87f8e657d01a0
ganm0r <gndhrvmv@gmail.com>
2022-03-13 20:32:33 +0530

    :sparkles: Add customised input component for chatbox

---
+++ src/components/InputChat/Input.tsx
@@ -0,0 +1,72 @@
+import React from "react";
+import styled, { css } from "styled-components";
+import theme from "../../theme";
+import { Stack } from "../Stack/Stack";
+
+const StyledInputTitle = styled.p(
+  () => css`
+    font-family: ${theme.fonts.primary};
+    font-weight: ${theme.fontWeights.extrabold};
+    font-size: 20px;
+    letter-spacing: 0.15em;
+    text-transform: uppercase;
+    color: ${theme.colors["purple-2"]};
+    margin-top: 5%;
+  `
+);
+
+const StyledInputField = styled.input(
+  () => css`
+    border-style: solid;
+    border-radius: 8px;
+    border-width: 3px;
+    min-width: 1000px;
+    padding: 12px;
+    box-sizing: border-box;
+    background-color: ${theme.colors["purple-0"]};
+    border-color: ${theme.colors["purple-2"]};
+    font-family: ${theme.fonts.primary};
+    font-weight: ${theme.fontWeights.bold};
+    font-size: 18px;
+    color: ${theme.colors["grey-1"]};
+
+    &:focus {
+      outline: none;
+      border-color: ${theme.colors["purple-2"]};
+    }
+  `
+);
+
+interface InputProps {
+  title: string;
+  type?: React.HTMLInputTypeAttribute;
+  placeholder?: string;
+  info?: string;
+  required?: boolean;
+  onChange?: (event: React.ChangeEvent<HTMLInputElement>) => void;
+}
+
+const Input = ({
+  title,
+  type,
+  placeholder,
+  required,
+  onChange,
+}: InputProps): JSX.Element => {
+  return (
+    <Stack direction="column" spacing={-2}>
+      <StyledInputTitle>{title}</StyledInputTitle>
+      <StyledInputField
+        type={type}
+        placeholder={placeholder}
+        spellCheck={false}
+        autoComplete="off"
+        required={required}
+        onChange={onChange}
+      ></StyledInputField>
+    </Stack>
+  );
+};
+
+export { Input };
+export type { InputProps };
